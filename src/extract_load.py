cfafca759db540000023f34d04471ed22d9461dc
Junior <jacsonsajr.study@gmail.com>
2025-05-09 18:09:09 -0300

    update libs

---
+++ src/extract_load.py
@@ -0,0 +1,12 @@
+#import das libs
+sho
+
+# import das minhas variaveis de ambiente
+
+
+# pegar a cotacao doa meus ativos
+
+
+#concatenar os meus ativos (1..2..3) -> (1)
+
+#salvear no banco de dados 
